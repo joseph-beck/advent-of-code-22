f04fbb0b2069757719a7b29129c39da1d3d4caba
joseph <96671050+joseph-beck@users.noreply.github.com>
2022-12-05 17:41:43 +0000

    rb broken

--- 5/rb/supply_stacks.rb
+++ 5/rb/supply_stacks.rb
@@ -3,7 +3,7 @@ require './stacks.rb'
 def run
     file_data = File.read('supply_stacks.txt').split("\n")
 
-    puts sort_stacks(file_data)
+    sort_stacks(file_data)
 end
 
 def sort_stacks(sorting_data)
@@ -14,10 +14,15 @@ def sort_stacks(sorting_data)
         instructions = line.split("\s")
         quantity, from, to = instructions[1].to_i, instructions[3].to_i - 1, instructions[5].to_i - 1
         
-        for i in 0..quantity do
-            stack[to].insert(0, stack[from].shift)
+        for _ in 0..quantity do
+            if stack[from] != ''
+                stack[from], popped = stack[from][1..-1], stack[from][0]
+                stack[to] = popped + stack[to]
+            end
         end
     end
 
-    stack
+    stack.each do |row|
+        puts row[0]
+    end
 end
